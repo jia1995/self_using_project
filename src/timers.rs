e68d95a132a45af9c76033aa505dfadbb3ca9b7d
jia1995 <15927447546@163.com>
2022-09-17 01:29:16 +0800

    [rust] implement timestamp to time with time zone

--- src/timers.rs
+++ src/timers.rs
@@ -1,7 +1,7 @@
 //! This lib is aiming at generate current time as i64.
 use std::time::SystemTime;
 #[derive(Debug)]
-pub struct DateTime (i16,u8,u8,u8,u8,u8,u8,u8,u8);
+pub struct DateTime (i16,u8,u8,u8,u8,u8,u16,u16,u16);
 
 impl Clone for DateTime {
     fn clone(&self) -> Self { todo!() }
@@ -12,15 +12,15 @@ impl Copy for DateTime {
 }
 
 impl DateTime {
-    pub fn new(year:i16, month:u8, day:u8, hour:u8, minute:u8, second:u8, millis:u8, macros:u8, nanos:u8) -> Self {
+    pub fn new(year:i16, month:u8, day:u8, hour:u8, minute:u8, second:u8, millis:u16, macros:u16, nanos:u16) -> Self {
         DateTime(year, month, day, hour, minute, second, millis, macros, nanos)
     }
 
-    fn leap_year(self, _year:i16) -> bool {
+    fn leap_year(_year:i16) -> bool {
         (_year%4==0 && _year%100!=0) || _year%400==0
     }
 
-    fn month_daily(self, _is_leap:bool) -> Vec<i8>{
+    fn month_daily(_is_leap:bool) -> Vec<i8>{
         if _is_leap {
             vec![31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         }else{
@@ -30,8 +30,8 @@ impl DateTime {
 
     pub fn time_zone(self, time_zone:i8) -> Self {
         let (mut _year,mut _month,mut _day,mut _hour) = (self.0, self.1 as i8, self.2 as i8, self.3 as i8);
-        let _is_leap = (&self).leap_year(_year);
-        let _month_day = (&self).month_daily(_is_leap);
+        let _is_leap = Self::leap_year(_year);
+        let _month_day = Self::month_daily(_is_leap);
         if time_zone < 0 {
             _hour += time_zone;
             let mut _minus_one = 0;
@@ -53,7 +53,7 @@ impl DateTime {
         } else {
             _hour += time_zone;
             let mut _add_one = 0;
-            if _hour > 24 {
+            if _hour >= 24 {
                 _add_one =1;
                 _hour %= 24;
             }
@@ -70,54 +70,7 @@ impl DateTime {
         DateTime(_year, _month as u8, _day as u8, _hour as u8, self.4, self.5, self.6, self.7, self.8)
     }
 
-    fn positive(time:i64) -> Self {
-        year += seconds/146097*400;
-        seconds %= 146097;
-        let mut num100year = seconds / 36524;
-        if num100year == 4 {
-            num100year -= 1;
-            seconds -= 36524*3;
-        }else{
-            seconds %= 36524;
-        }
-        year += num100year * 100;
-        year += seconds / 1461 * 4;
-        seconds %= 1461;
-        let mut num1year = seconds / 365;
-        if num1year == 4{
-            num1year -= 1;
-            seconds -= 365*3;
-        }else {
-            seconds %= 365;
-        }
-        year += num1year+1;
-        let mut month;
-        let mut day;
-        if seconds< 31{
-            month = 1;
-            day = seconds +1;
-        }else{
-            if (year%4==0 && year%100!=0) || year%400==0 {
-                seconds -= 29;
-                month = seconds*12 / 367;
-                day = seconds - month * 367 / 12;
-                if month== 0{
-                    day -= 1;
-                }
-            }else{
-                seconds -= 28;
-                month = seconds*12 / 367;
-                day = seconds - month * 367 / 12;
-                if month== 0{
-                    day -= 2;
-                }
-            }
-            month += 2;
-        }
-        DateTime(year as i16, month as u8, day as u8, hour as u8, mins as u8, secs as u8, 0,0,0)
-    }
-
-    fn nagative(time:i64) -> Self {
+    pub fn from(time: i64) -> Self {
         let (mut year,mut month,mut day,mut hour,mut minute,mut second) = (0,0,0,0,0,0);
         let mut seconds = time;
         if seconds >= 0 {
@@ -146,7 +99,7 @@ impl DateTime {
             }else {
                 seconds %= 365;
             }
-            year += num1year;
+            year += num1year + 1970;
             if seconds< 31{
                 month = 1;
                 day = seconds +1;
@@ -168,47 +121,144 @@ impl DateTime {
                 }
                 month += 2;
             }
-            year += 1970;
         } else {
             seconds *= -1;
-            
+            let mut _add_one = 0;
+            second = (60 - seconds%60)%60;
+            seconds /= 60;
+            if second>0 {
+                seconds += 1;
+                _add_one =1;
+            }
+            minute = (60 - seconds%60)%60;
+            seconds /= 60;
+            if minute > 0 {
+                _add_one = 1;
+                seconds += 1;
+            }
+            hour = (24 - seconds%24) % 24;
+            seconds/=24;
+            year += seconds/146097*400;
+            seconds %= 146097;
+            let mut num100year = seconds / 36524;
+            if num100year == 4 {
+                num100year -= 1;
+                seconds -= 36524*3;
+            }else{
+                seconds %= 36524;
+            }
+            year += num100year * 100;
+            year += seconds / 1461 * 4;
+            seconds %= 1461;
+            let mut num1year = seconds / 365;
+            if num1year == 4{
+                num1year -= 1;
+                seconds -= 365*3;
+            }else {
+                seconds %= 365;
+            }
+            year += num1year + 1;
+            year = 1970 - year;
+            let mut _day = 0;
+            let mut reverse_month_day = Self::month_daily(Self::leap_year(year as i16));
+            reverse_month_day.reverse();
+            month = 0;
+            for i in reverse_month_day {
+                if _day + (i as i64) < seconds{
+                    month += 1;
+                    _day +=(i as i64);
+                }else{
+                    day = (i as i64) - (seconds - _day);
+                    month = 12 - month;
+                    break;
+                }
+            }
         }
-        DateTime(year,month,day,hour,minute,second,0,0,0)
-    }
-
-    pub fn from(time: i64) -> Self {
-        if time >= 0 {
-            Self::positive(time)
-        } else{
-            Self::nagative(time)
-        }
+        DateTime(year as i16,month as u8,day as u8,hour as u8,minute as u8,second as u8,0,0,0)
     }
 
     pub fn from_millis(time: i64) -> Self {
-        let millis = time%1000;
-        let mut time = Self::from(time/1000);
-        time.6 = millis as u8;
-        time
+        let mut seconds = time;
+        let mut millis;
+        if seconds > 0{
+            millis = seconds%1000;
+            seconds /= 1000;
+        }else{
+            seconds *= -1;
+            millis = 1000 - seconds%1000;
+            seconds /= 1000;
+            if millis > 0 {
+                seconds += 1;
+            }
+            seconds *= -1;
+        }
+        let mut datetime = Self::from(seconds);
+        datetime.6 = millis as u16;
+        datetime
     }
 
     pub fn from_macros(time: i64) -> Self {
-        let macros = time%1_000;
-        let millis = (time/1_000)%1_000;
-        let mut time = Self::from(time/1_000_000);
-        time.6 = millis as u8;
-        time.7 = macros as u8;
-        time
+        let mut seconds = time;
+        let (mut millis, mut macros);
+        if seconds > 0{
+            macros = seconds%1000;
+            seconds /= 1000;
+            millis = seconds%1000;
+            seconds /= 1000;
+        }else{
+            seconds *= -1;
+            macros = 1000 - seconds%1000;
+            seconds /= 1000;
+            if macros > 0 {
+                seconds += 1;
+            }
+            millis = 1000 - seconds%1000;
+            seconds /= 1000;
+            if millis > 0 {
+                seconds += 1;
+            }
+            seconds *= -1;
+        }
+        let mut datetime = Self::from(seconds);
+        datetime.6 = millis as u16;
+        datetime.7 = macros as u16;
+        datetime
     }
 
     pub fn from_nanos(time: i64) -> Self {
-        let nanos = time%1_000;
-        let macros = (time/1_000)%1_000;
-        let millis = (time/1_000_000)%1_000;
-        let mut time = Self::from(time/1_000_000_000);
-        time.6 = millis as u8;
-        time.7 = macros as u8;
-        time.8 = nanos as u8;
-        time
+        let mut seconds = time;
+        let (mut millis, mut macros, mut nanos);
+        if seconds > 0{
+            nanos = seconds %1000;
+            seconds /= 1000;
+            macros = seconds%1000;
+            seconds /= 1000;
+            millis = seconds%1000;
+            seconds /= 1000;
+        }else{
+            seconds *= -1;
+            nanos = 1000 - seconds%1000;
+            seconds /= 1000;
+            if nanos > 0 {
+                seconds += 1;
+            }
+            macros = 1000 - seconds%1000;
+            seconds /= 1000;
+            if macros > 0 {
+                seconds += 1;
+            }
+            millis = 1000 - seconds%1000;
+            seconds /= 1000;
+            if millis > 0 {
+                seconds += 1;
+            }
+            seconds *= -1;
+        }
+        let mut datetime = Self::from(seconds);
+        datetime.6 = millis as u16;
+        datetime.7 = macros as u16;
+        datetime.8 = nanos as u16;
+        datetime
     }
 }
 
